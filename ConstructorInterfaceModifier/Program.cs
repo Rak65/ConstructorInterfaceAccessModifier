d3f1e3fd567e60ec4df307158da62c5a9bb0a11e
Rak65 <rakeshp6505@gmail.com>
2023-10-02 14:21:40 +0530

    [Rakesh] Add -: Constructor, Iterface and Access Modifier.

---
+++ ConstructorInterfaceModifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorInterfaceModifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Constructor, Iterface and Access Modifier.");
+        }
+    }
+}
